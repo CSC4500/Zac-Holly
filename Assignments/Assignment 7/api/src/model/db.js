1960c47e5132a17ee8c98ab3293d209f663d54cb
Zac Holly <zacholly@sbcglobal.net>
2021-11-27 17:47:45 -0600

    Add database connection logic

---
+++ Assignments/Assignment 7/api/src/model/db.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+// Retrieve local DB info
+const dbInfo = JSON.parse(fs.readFileSync("./authentication/db-info.json"));
+
+// Connect to DB from info
+const knex = require("knex")({
+    client: "mysql",
+    connection: dbInfo,
+});
+
+// Test DB connection
+knex
+    .raw("SELECT 1")
+    .timeout(1000)
+    .then(() => {
+        console.log("Database connection successful");
+    })
+    .catch((err) => {
+        knex.destroy();
+        console.log("Database connection ERROR");
+        throw err;
+    });
+
+module.exports = { knex };
